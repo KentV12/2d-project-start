3e5b82f93ba17720f58b17d3a835f92fa922fd0d
KentV12 <41877186+KentV12@users.noreply.github.com>
2024-04-30 20:25:26 -0500

    fixed camera

--- player.gd
+++ player.gd
@@ -15,7 +15,7 @@ func _physics_process(delta): # the update function
 	else:
 		$HappyBoo.play_idle_animation()
 		
-	const DAMAGE_RATE = 50.0
+	const DAMAGE_RATE = 10
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
