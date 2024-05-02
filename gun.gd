b6b6272c387ac8877b9d37c4c8c09c9d6adc9df8
KentV12 <41877186+KentV12@users.noreply.github.com>
2024-05-01 20:09:28 -0500

    Update gun.gd
    added 3x bullet cone spray

--- gun.gd
+++ gun.gd
@@ -9,10 +9,11 @@ func _physics_process(delta):
 
 func shoot():
 	const BULLET = preload("res://bullet.tscn") # load a bullet scene
-	var new_bullet = BULLET.instantiate()
-	new_bullet.global_position = %ShootingPoint.global_position
-	new_bullet.global_rotation = %ShootingPoint.global_rotation	
-	%ShootingPoint.add_child(new_bullet)
+	for angle in [-0.2, 0, 0.2]:
+		var new_bullet = BULLET.instantiate()
+		new_bullet.global_position = %ShootingPoint.global_position
+		new_bullet.global_rotation = %ShootingPoint.global_rotation	+ angle
+		%ShootingPoint.add_child(new_bullet)
 
 func _on_timer_timeout():
 	shoot()
